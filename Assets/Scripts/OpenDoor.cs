c02d8db8c43a90b362d58127b5686ca660ee11b4
SixtyGig <austin.winkler@cuw.edu>
2018-07-18 11:18:51 -0500

    First Escape Room

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public GameObject yellowWall;
+    public GameObject yellowKey;
+   
+    private void Awake()
+    {
+        this.yellowWall = GameObject.FindGameObjectWithTag("Yellow Wall");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Yellow Key")
+        {
+            this.yellowWall.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/OpenTheOtherDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTheOtherDoor : MonoBehaviour
+{
+    public GameObject redWall;
+    public GameObject redKey;
+
+    private void Awake()
+    {
+        this.redWall = GameObject.FindGameObjectWithTag("Red Wall");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Red Key")
+        {
+            this.redWall.SetActive(false);
+        }
+    }
+}
